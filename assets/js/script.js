f3970144082a34f7ceab123a7f6166ac4e5febb7
Jeffery Patterson <jpatterson933@ucla.edu>
2021-02-15 22:20:14 -0800

    added timer to js and header for timer and titles into html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,30 @@
+var bt1 = document.createElement("button"); //this will be the button that shows up to start the test
+var bt2 = document.querySelector("form > button");
+
+//these are my variables for the timer
+var timeLe = document.querySelector(".time");
+var secLe = 60;
+
+//this is my timer
+function setTime() {
+    var timerInterval = setInterval(function() {
+        secLe--;
+        timeLe.textContent = secLe + " is this working";
+
+        if(secLe === 0) {
+            clearInterval(timerInterval);
+            sendMessage();
+        }
+    }, 1000);
+}
+//this calls my function to start
+setTime();
+
 
 //this function should cycle the questions when the button is pressed
 //Problem -- function is not stopping and will blink rapidly
-function nextQuestion() {
-    
+//solution -- hold ctrl and then click on button and it works but opens a new window that is empty
+bt2.onclick = function nextQuestion() {
     //this variable selects all divs that are children to the id #questions
     var qTicker = document.querySelectorAll("#questions > div");
     //for statement is supposed to loop through the qTicker variable which is all children divs of parent #questions
@@ -12,10 +34,10 @@ function nextQuestion() {
             qTicker[i].style.display = "none";
             //if is value is the same as qTicker.length minus 1 then....
             if (i == qTicker.length - 1) {
-                //...display qticker array location 0
+                //...display qticker array location 0 as a block
                 qTicker[0].style.display = "block";
             } else {
-                //else display qticker loop + 1
+                //else display qticker loop + 1 as a block
                 qTicker[i + 1].style.display = "block";
             }
             //the break is meant to BREAK the loop which allows for code to be executed after
