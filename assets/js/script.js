5728609543c396c2c762f15c5110751b1dbcd281
Jeffery Patterson <jpatterson933@ucla.edu>
2021-02-16 21:36:10 -0800

    assets/

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,31 +4,43 @@ var timeLe = document.querySelector(".time");
 var secLe = 60;
 
 //i want to display 1 question at a time from my html class .quest when i press next question button
-var question = document.querySelectorAll(".quest");
+var pullQuestion = document.querySelectorAll(".quest");
 // i need to change my node list to an array
-var test = Array.from(question)
+var question = Array.from(pullQuestion)
 //i want to display one question from variable at a time from the test array breakdown further
 
 
+var i = Math.floor(Math.random() * question.length);
 //add event listener to next random question
-
-
-bt2.addEventListener("click", function nextQuestion(event){
+//if you take prevent default off here, it will switch to start, and then press again and it will show another question
+bt2.onclick = function test(event) {
     event.preventDefault();
-  
-    const random = Math.floor(Math.random() * test.length);
-    console.log(random, test[random]);
-    //now i want choices to not repeat
-    var x = test[random].style.display = "block";
 
-    
-})
+}
+
+bt1.onclick = function questionCycle(event) {
+    event.preventDefault();
+    question[i].style.display = "block";
+}
+
+
+//this is the next random question function and displays all questions on single page without cycling
+// bt2.addEventListener("click", function questionCycle(event){
+//     event.preventDefault();
+
+//     const random = Math.floor(Math.random() * question.length);
+//     console.log(random, question[random]);
+//     //var x makes questions appear in block format
+//     var x = question[random].style.display = "block";
+// })
+
+
+
 
 document.body.appendChild(bt1);
 
-bt1.addEventListener("click", function changeButton(event) {
+bt1.addEventListener("click", function buttonSwitch(event) {
     event.preventDefault();
-    event.stopPropagation();
     bt1.style.display = "none";
     bt2.style.display = "flex";
 
