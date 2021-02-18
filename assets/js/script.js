40f694a2a2206edc02c9d0188d4236866714a553
Jeffery Patterson <jpatterson933@ucla.edu>
2021-02-17 23:24:09 -0800

    added more notes to java and html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 
 //start button for quiz
 const start = document.getElementById("start");
+//timer position in html
+var timeEl = document.getElementById("quizTimer");
 //this represents the quiz div in html allowing us to place styles and stuff within
 const quiz = document.getElementById("quiz");
 //this is where the questions will get displayed
@@ -43,12 +45,35 @@ let questions = [
 const lasQuestion = questions.length - 1;
 //current question on screen variable set at 0 so when it increments in the renderQuestion function
 let runningQuestion = 0;
-//this is our timer variable set to count which is set to 0 //will adjust to take on a total amoutn of time later
-let count = 0;
 //this variable sets questiontime to 10 seconds on each question
 const questionTime = 10;
 //this is our score variable
 let score = 0;
+// time left variable
+var secondsLeft = 8;
+
+// quiz timer function
+function setQuizTime() {
+    var timerInterval = setInterval (function() {
+        secondsLeft--;
+        timeEl.textContent = secondsLeft + " seconds left!";
+        //this stops timer is seconds hits zero or the question is the last question
+        if(secondsLeft === 0 || runningQuestion == lasQuestion) {
+            clearInterval(timerInterval);
+            sendMessage();
+            //this 
+        }
+    }, 1000);
+}
+
+//function that sends message quiz over when the timer runs out
+function sendMessage() {
+    timeEl.textContent = "QUIZ OVER";
+}
+
+
+
+
 
 function renderQuestion() {
     //here we set the questions array to running question which is set at 0 above 
@@ -65,64 +90,48 @@ start.addEventListener("click", function startQuiz() {
     start.style.display = "none";
     renderQuestion();
     quiz.style.display = "block";
-    renderCounter();
-    TIMER = setInterval(renderCounter,1000);
+    //runs my timer function
+    setQuizTime();
 })
-//this function will render the counter and the final if statement will go to the next question if the counter hits the max
-//this is what we will use to control our time
-function renderCounter() {
-    if(count <= questionTime) {
-        counter.innerHTML = count;
-        count++
-    } else {
-        count = 0;
-        if(runningQuestion < lasQuestion){
-            runningQuestion++;
-            renderQuestion();
-    
-        } else{
-            //end of quiz
-            clearInterval(TIMER);
-            scoreRender();
-        }
-    }
-}
+
+
 //this function will check the answer and add to the score or decrease from the score
 //it will also reset the timer to 0 and cycle to the next question
 function checkAnswer(answer){
+    // says if the answer that was inputed is the same as the child .correct from parent questions(array of running questions) then it will 
+    //add 10 points to the score
     if(answer == questions[runningQuestion].correct) {
         //answer is correct
-        score++;
+        //this adds 10 to the score total - adds 10 to the left element -- thats what += means
+        score += 10;
+        //if the answer is not the same as in type or value to the child .correct of parent Questions[running questions ARRAY]
+        //then this will subtract from the score by 1
     } else {
         //here is where we would subtract questions from the score -- if you put score-- it will show a negative percentage
         //subtract from score
         score--;
     }
-    count = 0;
+    //running questions is an array and if that array index number is less than the last question
+    //increase the running array by 1 (go to the next questions) and perform function RenderQuestion
     if(runningQuestion < lasQuestion){
+        // increases runningQuestion array by 1
         runningQuestion++;
         renderQuestion();
+        //if the runningquestion array is not less than the last question then clear the timer
+        //and run the function scoreRender
     } else{
         //end of quiz
-        clearInterval(TIMER);
+        clearInterval(setQuizTime);
         scoreRender();
     }
 }
 
-function answerIsCorrect() {
-    document.getElementById(runningQuestion).style.backgroundcolor = "green";
-}
-function answerIsWrong() {
-    document.getElementById(runningQuestion).style.backgroundcolor = "red";
-}
 
 //score render
 function scoreRender() {
     scoreDiv.style.display = "block";
 
     //calculate the score - amount of questions answered by the user
-    const scorePercent = Math.round(100 * score/questions.length);
-    
-    scoreDiv.innerHTML = "<p>" + scorePercent + "%" + "</p>";
-
+    const scoreDisplay = score;
+    scoreDiv.innerHTML = "<p>" + scoreDisplay + "</p>";
 }
